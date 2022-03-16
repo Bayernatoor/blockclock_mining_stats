b2a8bf7dff0ce99e693fa715abacb294cd5bc588
Bayernator <bayernator@protonmail.com>
2022-03-16 12:10:26 +0100

    add comments, small fixes and modifications

--- blockclock_slushpool/blockclock_slushpool.py
+++ blockclock_slushpool/blockclock_slushpool.py
@@ -9,6 +9,7 @@ slush_pool_url = "https://slushpool.com/accounts/profile/json/btc/"
 headers = {"SlushPool-Auth-Token": "<YOURSLUSHPOOLAUTHTOKENGOESHERE>"}
 blockclock_ip = "<YOUBLOCKCLOCKIPGOESHERE>"
 
+# available blockclock tags  
 blockclock_tags = {
     "Estimated Hash Rate": "cm.mining.hash_rate_2016_blocks",
     "USD Market Price": "cm.markets.price",
@@ -21,6 +22,7 @@ blockclock_tags = {
     "Moscow Time": "memes.moscow.time"
 }
 
+# available slushpool tags
 slushpool_tags = {
     "Confirmed Reward": "confirmed_reward",
     "Unconfirmed Reward": "unconfirmed_reward",
@@ -41,13 +43,14 @@ def instructions():
     Enter the number 615 to skip to the next part or to end the selection process at any time.\n
     First list is Slushpool profile tags. Second list is Blockclock tags (COMING SOON). 
     Tags will appear in order selected and repeat continuously until the script is stopped (CTRL-C).
-    You may also select a refresh rate. Min: 5 minutes, Max: 10080 minutes (1 week, cause why not??!).   
+    You may also select a refresh rate for the blockclock. Min: 5 minutes, Max: 10080 minutes (1 week, cause why not??!).   
     <---------------------------->\n
           """)
     return 
 
 def display_refresh_time():
-    display_refresh = pyip.inputNum("Enter an update rate(5 - 999 minutes) ", min=5, max=10080)
+    # obtain an integer from the user to set display refresh rate
+    display_refresh = pyip.inputNum("Enter an update rate: (5 - 10080 minutes) ", min=5, max=10080)
     print(f"display will update every {display_refresh} minutes")  
     
     return int(display_refresh)
@@ -78,12 +81,15 @@ def display_refresh_time():
   
 def list_slushpool_tags():
   print("<--- Please select your Slushpool tags --->\n")
+  # list for user selected tags
   selected_tags_slushpool = []
   max_length_list_slush = len(slushpool_tags)
+  # get slushpool keys from dict and display to the user.
   slushpool_to_list = list(slushpool_tags.keys())
   for element in enumerate(slushpool_to_list):
       print(element) 
   print("\nTo Skip or move to the next step enter 615\n")
+  # loop until user selects desired tags, add them to a list.
   while len(selected_tags_slushpool) < max_length_list_slush:
       element = pyip.inputNum("Enter line number: ", min=0)
       if element == 615 or element > max_length_list_slush:
@@ -98,7 +104,7 @@ def get_slushpool_stats():
   # try GET request and catch any errors
   try:
     response = requests.get(slush_pool_url, headers=headers)
-    
+    # make sure it's a valid 200 respons
     if not response.status_code // 100 == 2:
       print(f"Error: Unexpected response {response}")
       print("Could not connect to slushpool\nTrying again in 15 seconds")
@@ -108,6 +114,7 @@ def get_slushpool_stats():
     return f"Error: {e}"
     
   try:
+    # change json response from slushpool to python Dict
     response_dict = json.loads(response.text)  
   except json.decoder.JSONDecodeError as e:
     return f"Json Error - check the URL/Parameters and try again.\nError: {e}"
@@ -116,14 +123,18 @@ def get_slushpool_stats():
   
  
 def format_slushpool_data():
+    # keep count so that we can continuously loop over the list of tags
     count = 0
     items_to_call = []
-    slushpool_query_dict = get_slushpool_stats()
+    # get user selection, get the appropriate key from the dict and add to the call list. 
     slushpool_tag_list = list_slushpool_tags() 
-    refresh_rate = (display_refresh_time() * 60)
     for item in slushpool_tag_list:
       result = slushpool_tags.get(item)
       items_to_call.append(result)
+    # Refresh rate of BlockClock display
+    refresh_rate = (display_refresh_time() * 60)
+    # run indefinitely and display selected tags based on refresh rate.
+    # different conditions based on tag used, different formating applied.
     while True:
        tag = items_to_call[count]
        if tag == 'ok_workers' or tag == 'off_workers':
@@ -139,6 +150,7 @@ def format_slushpool_data():
        elif tag == "hash_rate_5m" or tag == "hash_rate_60m" or tag == "hash_rate_24h" or tag == "hash_rate_scoring":
         slushpool_element = slushpool_query_dict["btc"][tag]
         formatted_hash_result = round(slushpool_element / 1000, 1)
+        # call function that send formatted tags to blockclock
         send_to_blockclock(formatted_hash_result, tag)
         print("Now Displaying: {tag}")
         if count >= len(items_to_call) -1:
@@ -149,6 +161,7 @@ def format_slushpool_data():
             time.sleep(refresh_rate)
        else: 
         slushpool_element = slushpool_query_dict["btc"][tag]
+        # format result to 7 digit precision
         formatted_result = "{:.7}".format(slushpool_element)
         send_to_blockclock(formatted_result, tag)
         print("Now Displaying: {tag}")
